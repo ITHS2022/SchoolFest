200c6ae3eca6963f9f0d19a10e541dfba04849ac
ITHS2022 <114249345+ITHS2022@users.noreply.github.com>
2022-10-06 10:25:51 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ if(stamp3 == "true") {
 }
 
 if(stamp4 == "true") {
-    document.stamp4.src = "https://github.com/ITHS2022/SchoolFest/blob/main/img/stamp/60th_logo_4.png?raw=true";
+    document.stamp4.src = "https://github.com/ITHS2022/SchoolFest/blob/main/img/stamp/60th_logo4.png?raw=true";
 }
 
 
